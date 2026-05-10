9713fa931eb0bdd512014619149e7b856c5fd6db
Krish Kumar Gupta <me.coder.in@gmail.com>
2026-05-10 15:26:03 +0530

    fix(types): cast req.params to string in packing controller

--- backend/src/controllers/packing.controller.ts
+++ backend/src/controllers/packing.controller.ts
@@ -4,35 +4,36 @@ import { sendSuccess, sendCreated, sendError } from '../utils/response';
 
 export const getPackingItems = async (req: Request, res: Response) => {
   try {
-    const data = await packingService.getPackingItems(req.params.id, req.user!.userId);
+    const data = await packingService.getPackingItems(req.params.id as string, req.user!.userId);
     sendSuccess(res, data);
   } catch (err: any) { sendError(res, err.message, err.status || 500); }
 };
 
 export const createPackingItem = async (req: Request, res: Response) => {
   try {
-    const item = await packingService.createItem(req.params.id, req.user!.userId, req.body);
+    const item = await packingService.createItem(req.params.id as string, req.user!.userId, req.body);
     sendCreated(res, item);
   } catch (err: any) { sendError(res, err.message, err.status || 500); }
 };
 
 export const togglePackingItem = async (req: Request, res: Response) => {
   try {
-    const item = await packingService.toggleItem(req.params.id, req.params.itemId, req.user!.userId);
+    const item = await packingService.toggleItem(req.params.id as string, req.params.itemId as string, req.user!.userId);
     sendSuccess(res, item, 200, 'Item toggled');
   } catch (err: any) { sendError(res, err.message, err.status || 500); }
 };
 
 export const deletePackingItem = async (req: Request, res: Response) => {
   try {
-    await packingService.deleteItem(req.params.id, req.params.itemId, req.user!.userId);
+    await packingService.deleteItem(req.params.id as string, req.params.itemId as string, req.user!.userId);
     sendSuccess(res, null, 200, 'Item deleted');
   } catch (err: any) { sendError(res, err.message, err.status || 500); }
 };
 
 export const resetPacking = async (req: Request, res: Response) => {
   try {
-    await packingService.resetAll(req.params.id, req.user!.userId);
+    await packingService.resetAll(req.params.id as string, req.user!.userId);
     sendSuccess(res, null, 200, 'Packing reset');
   } catch (err: any) { sendError(res, err.message, err.status || 500); }
 };
+
