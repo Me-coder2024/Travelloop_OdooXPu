aedadc4277326c0e80d58aa4a48cb982453f1fd1
Dixit Malviya <malviyadixit92@gmail.com>
2026-05-10 11:21:13 +0530

    feat: add global loading spinner for route transitions

---
+++ frontend/src/app/loading.tsx
@@ -0,0 +1,1 @@
+export default function Loading() { return (<div className='min-h-screen flex items-center justify-center'><div className='h-10 w-10 border-4 border-black border-t-transparent rounded-full animate-spin' /></div>); }
